8f847a059b434d1f0c2c864b82d7c4f790a36562
zayong <mase0804@naver.com>
2022-11-24 00:57:04 +0900

    today commit

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useParams, useLocation } from "react-router";
 import styled from "styled-components";
 
@@ -35,13 +35,24 @@ function Coin() {
 	const { coinId } = useParams<RouteParams>();
 	const [loading, setLoading] = useState(true);
 	const { state } = useLocation<RouteState>();
+	const [info, setInfo] = useState({});
+	const [priceInfo, setPriceInfo] = useState({});
+
+	useEffect(() => {
+		(async () => {
+			const infoData = await (await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)).json();
+			const priceData = await (await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)).json();
+			setInfo(infoData);
+			setPriceInfo(priceInfo);
+		})();
+	});
 
 	return (
 		<Container>
 			<Header>
 				<Title>{state?.name || "Loading..."}</Title>
 			</Header>
-			{loading ? <Loader>Loading...</Loader> : null}
+			{loading ? <Loader>Loading...</Loader> : <span>{info}</span>}
 		</Container>
 	);
 }
