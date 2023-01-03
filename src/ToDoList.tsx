06594ffb7f10a7ad8955daa6cca5ebbdef9f266f
zayong <mase0804@naver.com>
2023-01-03 23:32:32 +0900

    new update

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,42 +1,89 @@
-import React, { useState } from "react";
+import React from "react";
 import { useForm } from "react-hook-form";
 
-// function ToDoList() {
-// 	const [toDo, setToDo] = useState("");
-// 	const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-// 		const {
-// 			currentTarget: { value },
-// 		} = event;
-// 		setToDo(value);
-// 	};
-// 	const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-// 		event.preventDefault();
-// 		if (toDo.length < 10) {
-// 			return setToDoError("To do should be longer");
-// 		}
-// 		console.log(toDo);
-// 	};
-// 	return (
-// 		<div>
-// 			<form onSubmit={onSubmit}>
-// 				<input onChange={onChange} value={toDo} placeholder="Write a to do" />
-// 				<button>Add</button>
-// 			</form>
-// 		</div>
-// 	);
-// }
+/* function ToDoList() {
+  const [toDo, setToDo] = useState("");
+  const [toDoError, setToDoError] = useState("");
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    setToDoError("");
+    setToDo(value);
+  };
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (toDo.length < 10) {
+      return setToDoError("To do should be longer");
+    }
+    console.log("submit");
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input onChange={onChange} value={toDo} placeholder="Write a to do" />
+        <button>Add</button>
+        {toDoError !== "" ? toDoError : null}
+      </form>
+    </div>
+  );
+} */
+
+interface IForm {
+	email: string;
+	firstName: string;
+	lastName: string;
+	username: string;
+	password: string;
+	password1: string;
+}
 
 function ToDoList() {
-	const { register, handleSubmit } = useForm();
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<IForm>({
+		defaultValues: {
+			email: "@naver.com",
+		},
+	});
 	const onValid = (data: any) => {
 		console.log(data);
 	};
 	return (
 		<div>
-			<form onSubmit={handleSubmit(onValid)}>
-				<input {...register("Email")} placeholder="Email" type="text" />
-				<input {...register("Name")} placeholder="Name" type="text" />
-				<input {...register("Password")} placeholder="Password" type="text" />
+			<form style={{ display: "flex", flexDirection: "column" }} onSubmit={handleSubmit(onValid)}>
+				<input
+					{...register("email", {
+						required: "Email is required",
+						pattern: {
+							value: /^[A-Za-z0-9._%+-]+@naver.com$/,
+							message: "Only naver.com emails allowed",
+						},
+					})}
+					placeholder="Email"
+				/>
+				<span>{errors?.email?.message}</span>
+				<input {...register("firstName", { required: "write here" })} placeholder="First Name" />
+				<span>{errors?.firstName?.message}</span>
+				<input {...register("lastName", { required: "write here" })} placeholder="Last Name" />
+				<span>{errors?.lastName?.message}</span>
+				<input {...register("username", { required: "write here", minLength: 10 })} placeholder="Username" />
+				<span>{errors?.username?.message}</span>
+				<input {...register("password", { required: "write here", minLength: 5 })} placeholder="Password" />
+				<span>{errors?.password?.message}</span>
+				<input
+					{...register("password1", {
+						required: "Password is required",
+						minLength: {
+							value: 5,
+							message: "Your password is too short.",
+						},
+					})}
+					placeholder="Password1"
+				/>
+				<span>{errors?.password1?.message}</span>
 				<button>Add</button>
 			</form>
 		</div>
