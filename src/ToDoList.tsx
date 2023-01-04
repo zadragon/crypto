d58d05a890c9ec1affbd7f592f281ee29fe0b74f
zayong <mase0804@naver.com>
2023-01-05 00:40:41 +0900

    update

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -36,6 +36,7 @@ interface IForm {
 	username: string;
 	password: string;
 	password1: string;
+	extraError?: string;
 }
 
 function ToDoList() {
@@ -43,14 +44,19 @@ function ToDoList() {
 		register,
 		handleSubmit,
 		formState: { errors },
+		setError,
 	} = useForm<IForm>({
 		defaultValues: {
 			email: "@naver.com",
 		},
 	});
-	const onValid = (data: any) => {
-		console.log(data);
+	const onValid = (data: IForm) => {
+		if (data.password !== data.password1) {
+			setError("password1", { message: "Password are not the same" }, { shouldFocus: true });
+		}
+		// setError("extraError", { message: "Server offline." });
 	};
+	console.log(errors);
 	return (
 		<div>
 			<form style={{ display: "flex", flexDirection: "column" }} onSubmit={handleSubmit(onValid)}>
@@ -65,7 +71,16 @@ function ToDoList() {
 					placeholder="Email"
 				/>
 				<span>{errors?.email?.message}</span>
-				<input {...register("firstName", { required: "write here" })} placeholder="First Name" />
+				<input
+					{...register("firstName", {
+						required: "write here",
+						validate: {
+							noNico: (value) => (value.includes("nico") ? "no nicos allowed" : true),
+							noNick: (value) => (value.includes("nick") ? "no nick allowed" : true),
+						},
+					})}
+					placeholder="First Name"
+				/>
 				<span>{errors?.firstName?.message}</span>
 				<input {...register("lastName", { required: "write here" })} placeholder="Last Name" />
 				<span>{errors?.lastName?.message}</span>
@@ -85,6 +100,7 @@ function ToDoList() {
 				/>
 				<span>{errors?.password1?.message}</span>
 				<button>Add</button>
+				<span>{errors?.extraError?.message}</span>
 			</form>
 		</div>
 	);
